95404b12a5d6c040d46d42e201f65a879e63c54c
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-06 22:25:55 +0330

    feat: initial commit for database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+def create_connection():
+    try:
+        connection = psycopg2.connect(
+            database='your_database',
+            user='your_user',
+            password='your_password',
+            host='localhost',
+            port='5432'
+        )
+        return connection
+    except Exception as e:
+        print(f'Error connecting to database: {e}')
